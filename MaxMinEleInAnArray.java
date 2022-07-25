64b8ec85cf61c8e3c021f9593ec8bfa42c0a1392
heera9331 <71516269+heera9331@users.noreply.github.com>
2022-07-26 00:51:21 +0530

    Update MaxMinEleInAnArray.java

--- MaxMinEleInAnArray.java
+++ MaxMinEleInAnArray.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+class Main {
+    static void getMaxMin(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+        for(int i : nums) {
+            if(min > i) {
+                min = i;
+            }
+            if (max < i) {
+                max = i;
+            }
+        }
+        System.out.println("Max element: "+max);
+        System.out.println("Min element: "+min);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0; i<n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        getMaxMin(nums);
+    }
+}
 
